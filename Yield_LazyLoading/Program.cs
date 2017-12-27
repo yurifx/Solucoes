509026bfc7757e7f6f5e9b3d75949b1e63398326
Yuri Vasconcelos <yurifx@hotmail.com>
2017-12-27 21:05:27 -0200

    Yield explanation

---
+++ Yield_LazyLoading/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yield_LazyLoading
+{
+
+    /// <summary>
+    /// 
+    /// O Yield tem a função de atrasar o processamento (loop) de lista, caso você precise fazer o loop.
+    /// Ele é usado em iterações onde hajam retorno de IEnumerables, listas e afins (collections)
+    /// Podemos retornar um IEnumerable que só ira ser definitivamente carregado, quando acionarmos o .ToList();
+    /// Ou seja, mesmo chamando o método que contém o retorno Yield, ele só irá ser executado quando enumerarmos (tolist).
+    /// 
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lista = new List<Modelo> { new Modelo { Id = 1, Nome = "Yuri" },
+                                           new Modelo {Id = 2, Nome = "Teste"}
+                                         };
+
+
+            var instancia = new OperacaoYield();
+
+
+            //Com debug acionado, veja as execuções dos métodos abaixo:
+            var t1 = instancia.MetodoNormal(lista);
+            var t2 = instancia.MetodoYield(lista);
+
+            //o T2, não entra no processamento, até que seja chamado o método ToList();
+            t2.ToList();
+            
+
+            Console.ReadLine();
+
+
+        }
+    }
+
+    class OperacaoYield
+    {
+
+        public IEnumerable<Modelo> MetodoYield(IEnumerable<Modelo> lista)
+        {
+            foreach (var item in lista)
+            {
+                if (item.Id > 1)
+                    yield return item;
+            }
+        }
+
+        public IEnumerable<Modelo> MetodoNormal (IEnumerable<Modelo> lista)
+        {
+            var listaNova = new List<Modelo>();
+
+            foreach (var item in lista)
+            {
+                if (item.Id > 1)
+                    listaNova.Add(item);
+            }
+
+            return listaNova;
+        }
+
+    }
+
+
+
+
+
+    class Modelo
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+
+    }
+}
