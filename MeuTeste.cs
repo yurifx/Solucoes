282d6b71d9a75e6adecdd5cc490f64ad85a1eccb
yvs <yurifx@hotmail.com>
2018-02-07 19:00:53 -0200

    add tests with nUnit

---
+++ MeuTeste.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestesNUnit
+{
+    public class MeuTeste
+    {
+        [TestCase(1, 2, "3")]
+        [TestCase(1, 3, "4")]
+        [TestCase(1, 0, "1")]
+        public void MeuMetodoTeste(int valor, int valorb, string resultadoEsperado)
+        {
+            var p = new Program();
+            
+            var resultadoFuncao = p.Add(valor, valorb);
+
+            Assert.AreEqual(resultadoFuncao, resultadoEsperado);
+
+        }
+    }
+}
