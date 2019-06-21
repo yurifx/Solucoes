c87881d8efaa0176206c4a9a5c96e46a5c531bda
Yuri <yurifx@gmail.com>
2019-06-21 17:29:21 +1200

    Add files via upload

---
+++ Generics/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MeuMetodoGenericoSimples<String>();
+            MeuMetodoGenericoSimples<Int32>();
+            MetodoGenerico<Program>();
+            MetodoGenerico<Program>();
+
+
+            var classeGenericaString = new GenericClass<string>();
+            var classeGenericaInt = new GenericClass<Int32>();
+
+            classeGenericaString.DoSomething("valor");
+            classeGenericaInt.DoSomething(0);
+
+        }
+
+        private static void MeuMetodoGenericoSimples<T>()
+        {
+            Console.WriteLine(typeof(T).ToString());
+        }
+
+        static void MetodoGenerico<T>() where T: Program
+        {
+            Console.WriteLine(typeof(T).ToString());
+        }
+    }
+}
