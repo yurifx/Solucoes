57692f0035f19dbf538882a66254fbb79e4607c5
Yuri <yurifx@gmail.com>
2019-06-21 18:02:23 +1200

    Add files via upload

---
+++ ConstructorDestructor/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConstructorDestructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = new A();
+            var b = new B();
+        }
+    }
+
+    public class A
+    {
+        public A()
+        {
+            Console.WriteLine("Constructor A actioned");
+        }
+    }
+
+    public class B:A
+    {
+        public B():base() //this will call the constructor of the class A.
+        {
+            Console.WriteLine("Constructor B actioned");
+        }
+
+        ~B()
+        {
+            Console.WriteLine("Destructor Actioned");
+        }
+    }
+}
